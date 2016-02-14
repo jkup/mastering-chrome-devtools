0808ff74780d9f4e365f19ee7ecb7f18eca81de1
Jon Kuperman <jon.kuperman@gmail.com>
2016-02-13 18:24:19 -0800

    added debugging exercise

--- js/debugging.js
+++ js/debugging.js
@@ -1,1 +1,33 @@
-// Nothing here
+$("#github").on("keyup", function() {
+  var user = $("#github").val();
+
+  emptyCurrentList();
+
+  $.ajax({
+    url: "https://api.github.com/users/" + user + "/repos",
+    success: function(repos) {
+      iterateThroughData(repos);
+    },
+    error: function(data) {
+      insertError();
+    }
+  });
+});
+
+function iterateThroughData(repos) {
+  _.forEachRight(repos, function(repo) {
+    insertHTML(repo);
+  });
+}
+
+function insertHTML(repo) {
+  $("#repositories").append("<li>" + repo.title + "</li>");
+}
+
+function emptyCurrentList() {
+  $("#repositories").empty();
+}
+
+function insertError() {
+  $("#repositories").append("<li>Uh oh, we're having a problem!</li>");
+}
