5f0bbb8c6070e8b964a37bc980253c49d9d34cd0
Jon Kuperman <jon.kuperman@gmail.com>
2016-05-24 15:04:13 -0700

    added div necessary for profile

--- public/js/timing.js
+++ public/js/timing.js
@@ -1,7 +1,7 @@
-window.setTimeout(createDOMNodes, 1E3)
+window.setInterval(createDOMNodes, 1E3)
 
 function createDOMNodes() {
-  for (var i = 0; i < 1E3; i++) {
+  for (var i = 0; i < 100; i++) {
     var div = document.createElement('div')
     var divContent = document.createTextNode('Hello World')
     div.appendChild(divContent)
