8dc412eff488c1e500f2689490bf123685d10f2a
Jon Kuperman <jon.kuperman@gmail.com>
2016-07-31 21:02:13 -0700

    added keys to avoid rate limits

--- public/js/debugging.js
+++ public/js/debugging.js
@@ -5,7 +5,7 @@ $("#github").on("submit", function() {
 
   // Using the github API https://developer.github.com/v3/repos/#list-user-repositories
   $.ajax({
-    url: "https://api.github.com/users/" + user + "/repos",
+    url: "https://api.github.com/users/" + user + "/repos?client_id=1699520b1a1353e2d28f&client_secret=19f0b5d59aa52197d1c3e59ce8d85233738cda20",
     success: function(repos) {
       iterateThroughData(repos);
     },
