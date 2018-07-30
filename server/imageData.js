9fe7f9d8575ce783c832e0dd29119f0c77dc15dd
Jon Kuperman <jon.kuperman@gmail.com>
2018-07-29 20:37:54 -0700

    add debug example

---
+++ server/imageData.js
@@ -0,0 +1,24 @@
+module.exports = {
+  images: [
+    {
+      name: "Stormtrooper on planter",
+      photographer: "Ciprian Boiciuc",
+      source: "/img/robot1.jpg"
+    },
+    {
+      name: "Lego, stormtrooper, marco and star war",
+      photographer: "Daniel Cheung",
+      source: "/img/robot2.jpg"
+    },
+    {
+      name: "Lego, stormtrooper, marco and star war",
+      photographer: "Daniel Cheung",
+      source: "/img/robot3.jpg"
+    },
+    {
+      name: "Hand, sketchpad, drawing and flatlay",
+      photographer: "rawpixel",
+      source: "/img/robot4.jpg"
+    }
+  ]
+};
---
+++ server/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const imageData = require("./imageData");
+// const compression = require('compression')
+const app = express();
+
+app.set("view engine", "pug");
+
+// app.use(compression())
+app.use(express.static("public"));
+
+// Home Page
+app.get("/", function(req, res) {
+  res.render("index", {
+    scripts: ["main.js"]
+  });
+});
+
+// Editing Demo
+app.get("/editing", function(req, res) {
+  res.render("editing", {
+    scripts: ["editing.js"]
+  });
+});
+
+// Debugging Demo
+app.get("/debugging", function(req, res) {
+  res.render("debugging", {
+    scripts: ["debugging.js"]
+  });
+});
+
+// Debugging API Endpoint
+app.get("/api", function(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(imageData));
+});
+
+// Profiling Demo
+app.get("/profiling", function(req, res) {
+  res.render("profiling", {
+    scripts: ["profiling.js"]
+  });
+});
+
+// Timing Demo
+app.get("/timing", function(req, res) {
+  res.render("timing", {
+    scripts: ["timing.js"]
+  });
+});
+
+// Page Jank Demo
+app.get("/page-jank", function(req, res) {
+  res.render("page-jank");
+});
+
+// Memory Leaks
+app.get("/memory-leaks", function(req, res) {
+  res.render("memory-leaks/index", {
+    scripts: ["memory-leaks.js"]
+  });
+});
+
+app.get("/memory-leaks/accidental-globals", function(req, res) {
+  res.render("memory-leaks/accidental-globals", {
+    scripts: ["accidental-globals.js"]
+  });
+});
+
+app.get("/memory-leaks/callbacks-and-timers", function(req, res) {
+  res.render("memory-leaks/callbacks-and-timers");
+});
+
+app.get("/memory-leaks/forgotten-dom", function(req, res) {
+  res.render("memory-leaks/forgotten-dom");
+});
+
+app.get("/memory-leaks/closures", function(req, res) {
+  res.render("memory-leaks/closures");
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port http://localhost:3000");
+});
